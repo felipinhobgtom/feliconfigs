6ce4cb40ad6d65c3e4e7a19e8d3f22fabd3d3e04
felipinho <felipinhobgouveia@gmail>
2025-09-13 22:27:56 -0300

    few fixes and colorscheme changes

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,22 @@
 return {
-  "ellisonleao/gruvbox.nvim",
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "gruvbox",
+  "wtfox/jellybeans.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    transparent = true,
+    bold = true,
+    italic = true,
+    plugins = {
+      all = false,
+      auto = true, -- will read lazy.nvim and apply the colors for plugins that are installed
+    },
+    background = {
+      dark = "jellybeans", -- default dark palette
+      light = "jellybeans_light", -- default light palette
     },
   },
+  config = function(_, opts)
+    require("jellybeans").setup(opts)
+    vim.cmd.colorscheme("jellybeans")
+  end,
 }
