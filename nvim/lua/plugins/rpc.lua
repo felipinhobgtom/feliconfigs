240b53b38fbbf4c070cc733815c66a5772e42517
felipinho <felipinhobgouveia@gmail>
2025-09-14 13:03:13 -0300

    feat: discord rpc plugin

---
+++ nvim/lua/plugins/rpc.lua
@@ -0,0 +1,9 @@
+return {
+  "andweeb/presence.nvim",
+  opts = {
+    main_image = "file",
+    neovim_image_text = "prob not really coding",
+    plugin_manager_text = "nvim ricing?",
+    buttons = true,
+  },
+}
